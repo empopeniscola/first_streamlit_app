4c3a9c088da6d46ecb4a72512788e4161b057fe6
empopeniscola <117941121+empopeniscola@users.noreply.github.com>
2022-11-21 20:26:08 +0100

    Update streamlit_app.py
    Removed the streamlit.stop command

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,9 +61,6 @@ if streamlit.button('Get Fruit Load List'):
    my_data_rows = get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
-# Don't run anything past here while we troubleshoot
-streamlit.stop()
-
 # Allow the end user to add a fruit to the list
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the list'):
